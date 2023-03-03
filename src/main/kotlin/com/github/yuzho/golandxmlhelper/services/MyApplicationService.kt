cdfad1e4a3058863fa71e44bd0c1ed1a1cf5b6bf
GitHub Action <action@github.com>
2023-03-03 03:15:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/yuzho/golandxmlhelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yuzho/golandxmlhelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yuzho.golandxmlhelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yuzho.golandxmlhelper.MyBundle
 
 class MyApplicationService {
 
